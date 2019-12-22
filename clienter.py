d6e9919ed34f47ba584a0eed649d127c901402e8
dmfananddu <af0706@yandex.ru>
2019-12-22 18:45:42 +0300

    simulation begining

--- clienter.py
+++ clienter.py
@@ -19,44 +19,50 @@ class Client(object):
             который пользователь посмотрел последним, начинает смотреть следующий урок
     """
 
-    def __init__(self, themes, subs=[], content=[]):
-        if len(themes) > 3:
-            self.current_themes = []
-            while len(self.current_themes) < 3:
-                tmp_theme = choice(themes)
-                if tmp_theme not in self.current_themes:
-                    self.current_themes.append(tmp_theme)
-        else:
-            self.current_themes = themes
-        
+    def __init__(self, themes, content=[]):
+        self.id = hash(randint(0, 10000000) + randint(0, 10000000))
+        self.current_themes = []
+        while len(self.current_themes) < 3:
+            tmp_theme = choice(themes)
+            if tmp_theme not in self.current_themes:
+                self.current_themes.append(tmp_theme)
         self.sub_list_len = 0
         self.current_subscriptions = []
 
-        if not subs and content != []:
+        if content != []:
             self.sub_list_len = randint(0,3)
-            for i in range(len(self.sub_list_len)):
+            tmp_content = []
+            while len(tmp_content) != self.sub_list_len:
                 tmp_cont_type = randint(0, 1)
                 if tmp_cont_type == 0:
-                    tmp_cont_number = randint(0, len(content.manual_list))
-                    tmp_cont = content.manual_list[tmp_cont_number]
+                    tmp_manual_list = []
+                    for i in range(len(content.manual_list)):
+                        if content.manual_list[i].theme in self.current_themes:
+                            tmp_manual_list.append(content.manual_list[i])
+                    tmp_cont_number = randint(0, len(tmp_manual_list)-1)
+                    tmp_cont = tmp_manual_list[tmp_cont_number]
                 else:
-                    tmp_cont_number = randint(0, len(content.course_list))
-                    tmp_cont = content.course_list[tmp_cont_number]
-                    tmp_cont = [randint(0, len(tmp_cont)-1), tmp_cont]
+                    tmp_course_list = []
+                    for i in range(len(content.course_list)):
+                        if content.course_list[i].theme in self.current_themes:
+                            tmp_course_list.append(content.course_list[i])
+                    tmp_cont_number = randint(0, len(tmp_course_list)-1)
+                    tmp_cont = tmp_course_list[tmp_cont_number]
+                    tmp_cont = [tmp_cont, randint(0, len(tmp_cont.lessons)-1)]
+                tmp_content.append(tmp_cont)
 
-                self.current_subscriptions.append(tmp_cont)
-
-        self.id = hash(randint(0, 10000000) + randint(0, 10000000))
+            self.current_subscriptions = tmp_content
+        
         self.statistics = {
-            'id': self.id,
-            'went to portal': None,
-            'out of portal': None,
-            'spend time': None,
+            'ID': self.id,
+            'wentToPortal': None,
+            'outOfPortal': None,
+            'MPST': randint(1, 48) * 30,
             'content': None,
-            'studied resources': [{
+            'studiedResources': [{
                 'resource': None,
-                'start time': None,
-                'end time': None
+                'ST': None,
+                'ET': None
             }], # ресурсы, которые были полностью изучены
         }
 
--- simulation.py
+++ simulation.py
@@ -3,21 +3,76 @@ from clienter import Client
 import pickle
 from contenter import Content
 import datetime
-from random import randint
+from random import randint, choice
 
 
 COURSE_COUNT = 7
 MANUAL_COUNT = 10
-CLIENT_COUNT = 3500
-CLIENT_GEN_TIMEOUT = 0.5
+CLIENT_COUNT = 100
+CLIENT_GEN_TIMEOUT = 5
+SERVER_COUNT = 5
 
 
 class ClientSim(object):
-    def __init__(self, env, sub_list: list, content: Content):
-        self.client = Client(sub_list, content)
+    def __init__(self, env, theme_list: list, content: Content):
+        self.client = Client(theme_list, content)
         self.env = env
         self.content = content
         self.action = env.process(self.run())
 
     def run(self):
-        pass
+        with server.request() as req:
+            self.client.statistics['wentToPortal'] = self.env.now
+            yield req
+            yield self.env.process(self.watch_content())
+
+    def watch_content(self):
+        yield self.env.timeout(0.00001)
+    
+    def new_now(self, now):
+        return datetime.datetime.combine(datetime.date.today() +
+            datetime.timedelta(days=(round(now) // 60) // 24),  # точный день
+            datetime.time((round(now) // 60) % 24, round(now) % 60)
+        )
+    
+    def choice_content_element(self):
+        for i in self.client.current_subscriptions:
+            course_dict = dict([(sub.name, sub) for sub in self.content.course_list])
+            manual_dict = dict([(sub.name, sub) for sub in self.content.course_list])
+            if i.name in course_dict.keys():
+                return course_dict[i.name]
+            if i.name in manual_dict.keys():
+                return manual_dict[i.name]
+        return None         # возвращаем пустоту, если для клиента нет подходящего фильма
+
+
+
+daily_content = Content(MANUAL_COUNT, COURSE_COUNT)
+print(daily_content)
+
+def get_theme_list(filename='themes.txt'):
+    with open(filename, 'r', encoding='utf-8') as reader:
+        themes_list = []
+        for theme in reader:
+            themes_list.append(theme.strip('\n'))
+    return themes_list
+
+
+env = simpy.Environment()
+server = simpy.Resource(env, capacity=SERVER_COUNT)
+themes = get_theme_list()
+clients = []
+def client_creating(env, clients):
+    for i in range(CLIENT_COUNT):
+        client = ClientSim(env, themes, daily_content)
+        clients.append(client)
+        yield env.timeout(CLIENT_GEN_TIMEOUT)
+env.process(client_creating(env, clients))
+env.run(until=1440) # сутки моделировали-моделировали, да не вымоделировали
+
+# Перенос статистики в файл
+statistic = []
+for i in range(clients.__len__()):
+    statistic.append(clients[i].client.statistics)
+with open('sim.pickle', 'wb') as writer:
+    pickle.dump(statistic, writer)
